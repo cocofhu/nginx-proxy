4a62ee0584339f674a0b3cd9290d56c56940f5fc
cocofhu <cocofhu@tencent.com>
2025-09-15 22:36:22 +0800

    ci

--- internal/db/driver.go
+++ internal/db/driver.go
@@ -4,15 +4,19 @@ import (
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
+	_ "modernc.org/sqlite" // 纯 Go SQLite 驱动
 )
 
 // InitDB 初始化数据库连接
 // 使用纯 Go SQLite 驱动，避免 CGO 编译问题
 func InitDB(dbPath string) (*gorm.DB, error) {
-	// 使用 GORM 官方的 SQLite 驱动，配置为纯 Go 模式
+	// 使用纯 Go SQLite 驱动
 	dsn := dbPath + "?_pragma=foreign_keys(1)&_pragma=journal_mode(WAL)&_pragma=synchronous(NORMAL)"
 
-	db, err := gorm.Open(sqlite.Open(dsn), &gorm.Config{
+	db, err := gorm.Open(sqlite.Dialector{
+		DriverName: "sqlite", // 使用 modernc.org/sqlite 驱动
+		DSN:        dsn,
+	}, &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
 	})
 	if err != nil {
